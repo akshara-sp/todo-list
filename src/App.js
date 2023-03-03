d5b6ac943c76f62f68b0d4700f4132932b474984
akshara-sp <95979133+akshara-sp@users.noreply.github.com>
2023-03-02 23:28:12 -0800

    local todo list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import './App.css';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import Todo from './components/Todo';
 
 function App() {
+    const [todos, setTodos] = useState([
+        'Make a react-based todo list project',
+        'Deploy it on firebase'
+    ])
+    const [input, setInput] = useState('')
+    const addTodo = e => {
+        e.preventDefault()
+        setTodos([...todos, input])
+        setInput('')
+    }
+
     return (
-        <div className="App">
-        <h1>ToDo List</h1>
+        <div className="app">
+            <h1>ToDo List</h1>
+            <form>
+                <FormControl>
+                    <InputLabel>Write a TODO</InputLabel>
+                    <Input value={input} onChange={e => setInput(e.target.value)}/>
+                </FormControl>
+                <Button type="submit" onClick={addTodo} variant="contained" color="primary" disabled={!input}>Add Todo</Button>
+            </form>
+            <ul>
+                {todos.map(todo => <Todo todo={todo} />)}
+            </ul>
         </div>
     );
 }
+
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import { List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core'
+import React from 'react'
+
+const Todo = ({ todo }) => {
+    return (
+        <List className="todo__list">
+            <ListItem>
+                <ListItemAvatar />
+                <ListItemText primary={todo} secondary={todo} />
+            </ListItem>
+        </List>
+    )
+}
+export default Todo
