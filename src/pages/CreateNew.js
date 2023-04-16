a1a8bed205f4676aa1249e39cd12863f986d64d9
akshara-sp <95979133+akshara-sp@users.noreply.github.com>
2023-04-16 04:50:04 -0700

    Update create new form

--- src/pages/CreateNew.js
+++ src/pages/CreateNew.js
@@ -1,34 +1,99 @@
-import { useState } from 'react';
-import { Button, FormControl, Input, InputLabel } from '@mui/material';
+
+import React, { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import firebase from 'firebase/compat/app';
-import { db } from '../firebase_config';
-import './CreateNew.css';
+import { db, auth } from '../firebase_config';
+import { useNavigate } from 'react-router-dom'
+import { Typography } from '@mui/material';
 
-function CreateNew() {
-    const [input, setInput] = useState('')
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 2,
 
-    const addTodo = e => {
-        e.preventDefault()
-        if (input.trim().length > 0) {
-            db.collection('todos').add({
-                todo: input.trim(),
-                timestamp: firebase.firestore.FieldValue.serverTimestamp()
-            })
-        }
-        setInput('')
+    '& .MuiTextField-root': {
+      margin: 1,
+      width: '300px',
+    },
+    '& .MuiButtonBase-root': {
+      margin: 2,
+    },
+  },
+}));
+
+const CreateNew = () => {
+  const classes = useStyles();
+  // create state variables for each input
+  const [todo, setTodo] = useState('');
+  const [description, setDescription] = useState('');
+  const [due, setDue] = useState('');
+
+  let navigate = useNavigate();
+
+  const user = auth.currentUser;
+  console.log("current user")
+  console.log(user);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // console.log(email, password);
+    console.log(due)
+    if (todo.trim().length > 0) {
+        db.collection('todos').add({
+            todo: todo.trim(),
+            description: description.trim(),
+            due: due,
+            userid: user.uid,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            status: 'created'
+        })
+        setTodo('')
+        setDescription('')
+        setDue('')
+        // navigate('/')
     }
+  };
 
-    return (
-        <div className="create_new">
-            <form className='todo_form'>
-                <FormControl>
-                    <InputLabel>Write a To-Do</InputLabel>
-                    <Input value={input} onChange={e => setInput(e.target.value)}/>
-                </FormControl>
-                <Button type="submit" onClick={addTodo} variant="contained" color="primary" disabled={!input}>Add To-do</Button>
-            </form>
-        </div>
-    );
-}
+  return (
+    <form className={classes.root} onSubmit={handleSubmit}>
+      <Typography variant="h6">
+            Write a To-Do
+      </Typography>
+      <TextField
+        label="To-Do"
+        variant="outlined"
+        type="text"
+        required
+        value={todo}
+        onChange={e => setTodo(e.target.value)}
+      />
+      <TextField
+        label="Description"
+        variant="outlined"
+        type="text"
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+      <TextField
+        label="Due"
+        variant="outlined"
+        type="datetime-local"
+        value={due}
+        required
+        onChange={e => setDue(e.target.value)}
+      />
+      <div>
+        <Button type="submit" variant="contained" color="primary">
+          Create New To-Do
+        </Button>
+      </div>
+    </form>
+  );
+};
 
 export default CreateNew;
